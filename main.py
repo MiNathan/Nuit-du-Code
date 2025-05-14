58fc401c4fc52cbfa7f0bf7a48cbd29a577aae2f
MiNathan <nathan.theault@gmail.com>
2025-05-14 16:07:01 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pyxel as p
 import random as r
 
-allies = {"fregate": [], "destroyer": [], "porte-vaisseau": [], "sous-vaisseau": []} # Vaisseaux allie
-ennemis = {"fregate": [], "destroyer": [], "porte-vaisseau": [], "sous-vaisseau": []} # Vaisseaux ennemis
+allies = [[], [], [], []] # Vaisseaux allie
+ennemis = [[], [], [], []]# Vaisseaux ennemis
 
 
 class UI:
@@ -55,12 +55,11 @@ class Vaisseau:
         self.equipe = equipe
         self.x = x
         self.y = y
-        self.cible = None
+        self.cible = self
         if type == "fregate":
             self.pv = 20
             self.spd_atk = 120 # Attaque toutes les 2 secondes
             self.str_atk = 3 # Les attaques font 1 dégats
-            self.aa = False
             self.cap = 0
             self.coords = (0, 0, 32, 16) # Les coordonées dans le fichier pyxel
 
@@ -68,7 +67,6 @@ class Vaisseau:
             self.pv = 30
             self.spd_atk = 300 # Attaque toutes les 5 secondes
             self.str_atk = 5 # Les attaques font 2 dégats
-            self.aa = True
             self.cap = 0
             self.coords = (0, 0, 32, 16) # Les coordonées dans le fichier pyxel
 
@@ -76,7 +74,6 @@ class Vaisseau:
             self.pv = 50
             self.spd_atk = 0 # N'attaque pas directement
             self.str_atk = 0
-            self.aa = False
             self.cap = 3
             self.coords = (0, 0, 32, 16) # Les coordonées dans le fichier pyxel
 
@@ -84,7 +81,6 @@ class Vaisseau:
             self.pv = 5
             self.spd_atk = 180 # Attaque toutes 3 les secondes
             self.str_atk = 2 # Les attaques font 2 dégats
-            self.aa = True
             self.cap = 0
             self.coords = (0, 0, 32, 16) # Les coordonées dans le fichier pyxel
 
@@ -92,9 +88,10 @@ class Vaisseau:
             self.pv = 5
             self.spd_atk = 30 # Attaque 2 fois par secondes
             self.str_atk = 1 # Les attaques font 1 dégats
-            self.aa = False
             self.cap = 0
             self.coords = (0, 0, 32, 16) # Les coordonées dans le fichier pyxel
+
+
     def get_pv(self):
         return self.pv
     
@@ -123,24 +120,22 @@ class Vaisseau:
         self.coule()
 
     def attaquer(self, cible):
-        cible.prends_degats(self.str_atk)
+        self.cible.prends_degats(self.str_atk)
 
     def selection_cible(self):
         if self.equipe == "allies":
-            if self.aa:
-                i_tab = r.randint(0, 3)
-                self.cible = r.choice(ennemis[i_tab])
+            i_tab = 0
+            if ennemis[i_tab] == [] and i_tab < len(ennemis[i_tab]):
+                i_tab += 1
             else:
-                i_tab = r.randint(0, 2)
                 self.cible = r.choice(ennemis[i_tab])
 
         if self.equipe == "ennemis":
-            if self.aa:
-                i_tab = r.randint(0, 3)
-                self.cible = r.choice(ennemis[i_tab])
+            i_tab = 0
+            if allies[i_tab] == [] and i_tab < len(allies[i_tab]):
+                i_tab += 1
             else:
-                i_tab = r.randint(0, 2)
-                self.cible = r.choice(ennemis[i_tab])
+                self.cible = r.choice(allies[i_tab])
 
     def draw_vais(self):
         if self.equipe == "allies":
@@ -149,36 +144,41 @@ class Vaisseau:
             p.blt(self.x, self.y, 1, self.coords[0], self.coords[1], self.coords[2], self.coords[3], 0)
 
 
-
-
-
-
-
-
-
 class App:
     def __init__(self):
         p.init(width=256, height=256, title="Bataille Spatiale", fps=60)
         p.load("theme.pyxres")
         p.mouse(True)
-        allies["fregate"].append(Vaisseau("fregate", "allies", 4, 236))
-        ennemis["fregate"].append(Vaisseau("fregate", "allies", 4, 4))
+        allies[0].append(Vaisseau("fregate", "allies", 4, 236))
+        allies[0].append(Vaisseau("fregate", "allies", 40, 236))
+        allies[0].append(Vaisseau("destroyer", "allies", 4, 206))
+        ennemis[0].append(Vaisseau("fregate", "allies", 4, 4))
+        ennemis[0].append(Vaisseau("fregate", "allies", 40, 4))
 
         p.run(self.update, self.draw)
 
     def update(self):
-        for tab in allies.values():
+        for tab in allies:
             for vais in tab:
-                vais.selection_cible()
+                if vais.get_cible().coule():
+                    vais.selection_cible()
+
+        for tab in allies:
+            for vais in tab:
+                vais.attaquer(vais.get_cible)
+                print(vais.get_cible())
+        for tab in ennemis:
+            for vais in tab:
+                vais.attaquer(vais.get_cible)
 
     def draw(self):
         p.cls(0)
         p.line(0, 127, 256, 127, 1)
         p.line(0, 128, 256, 128, 1)
-        for tab in allies.values():
+        for tab in allies:
             for vais in tab:
                 vais.draw_vais()
-        for tab in ennemis.values():
+        for tab in ennemis:
             for vais in tab:
                 vais.draw_vais()
 
